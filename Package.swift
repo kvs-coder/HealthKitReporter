67a815a4be0347d72507a9bec414a9c4add63eea
kvs <victorkachalov@gmail.com>
2021-05-27 08:58:54 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HealthKitReporter",
-            path: "HealthKitReporter/Classes"),
+            path: "Sources"),
     ]
 )
 
