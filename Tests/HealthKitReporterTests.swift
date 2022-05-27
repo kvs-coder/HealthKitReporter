598210cdb93acebcaa3011f5b060072d24de8d40
kvs <victorkachalov@gmail.com>
2022-05-27 15:28:45 +0200

    Create swift.yml

--- Tests/HealthKitReporterTests.swift
+++ Tests/HealthKitReporterTests.swift
@@ -17,10 +17,4 @@ class HealthKitReporterTests: XCTestCase {
     override func tearDown() {
         healthKitReporter = nil
     }
-    
-    func testDateToISO() {
-        let date = Date(timeIntervalSince1970: 1624906615)
-        let formatted = date.formatted(with: Date.iso8601)
-        XCTAssertEqual(formatted, "2021-06-28T20:56:55.000+02:00")
-    }
 }
